67bcb006ea5637678373a9f95cbd377c474cc99f
ainetta12 <120013205+ainetta12@users.noreply.github.com>
2023-12-11 13:00:06 +0100

    scene

--- Assets/Scripts/RayCast.cs
+++ Assets/Scripts/RayCast.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RayCast : MonoBehaviour
 {
 
-    public Camera camaraPrincipal;
+    public Camera camera;
 
 
     // Update is called once per frame
@@ -19,7 +20,7 @@ public class RayCast : MonoBehaviour
 
     void Rayo()
     {
-        Ray ray = camaraPrincipal.ScreenPointToRay(Input.mousePosition);
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitRayo;
 
         if(Physics.Raycast(ray, out hitRayo, Mathf.Infinity))
@@ -29,21 +30,25 @@ public class RayCast : MonoBehaviour
             if(hitRayo.transform.gameObject.layer == 6)
             {
                 Debug.Log("hit cube1");
-                
+                SceneManager.LoadScene(1);
                 
             }
 
             if(hitRayo.transform.gameObject.layer == 7)
             {
                 Debug.Log("hit cube2");
+                SceneManager.LoadScene(2);
                 
             }
 
             if(hitRayo.transform.gameObject.layer == 8)
             {
                 Debug.Log("hit sphere");
+                SceneManager.LoadScene(3);
                 
             }
+
+            
         } 
     }
 
